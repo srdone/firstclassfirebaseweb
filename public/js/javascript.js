938f54c1fe2a9680bc9ae57ab2cae2783d50b1b9
Stephen Done <srdone@gmail.com>
2015-01-22 21:10:41 -0700

    Firebase app - trying to get a method on an object to work

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -11,11 +11,61 @@ var scout;
 
 
 scoutApp.controller('ScoutCtrl', ["$scope", "$firebase", 
-  function($scope, $firebase) {
-    var scoutFb = new Firebase("https://blazing-heat-189.firebaseio.com/scout/");
+  function ($scope, $firebase) {
+    var scoutFb = new Firebase("https://blazing-heat-189.firebaseio.com/scouts/0/");
+    var awardsFb = new Firebase("https://blazing-heat-189.firebaseio.com/awards/");
     
     var scoutSync = $firebase(scoutFb);
+    var awards = $firebase(awardsFb).$asObject;
     
     $scope.scout = scoutSync.$asObject();
+    
+    // This function is not currently working - why?
+    $scope.scout.currentRank = function() {
+      function getAwardReqs(awardId) {
+        awards.forEach(function(current) {
+          if (current.awardId === awardId) {
+            return current.reqId; 
+          }
+        });
+      }
+      
+      function isComplete(award) {
+        var awardId = award.awardId;
+        var awardReqs = getAwardReqs(awardId);
+        var competedReqs = getCompletedReqs(awardId);
+        
+        for (var i = 0; i < awardReqs.length; i++) {
+           if (competedReqs.indexOf(awardReqs[i]) === -1) {
+             return false;
+           }
+        }
+        return true;
+      }
+      
+      var awardsStarted = this.requirementsCompleted.map(function(current) {
+        var getAwardObj = function(awardId) {
+          for (var i = 0; i < awards.length; i++) {
+            if (awards[i].awardId === awardId) {
+              return awards[i]; 
+            }
+          }
+        }
+        
+        var award = getAwardObj(current.awardId);
+        alert(award.awardName);
+        return award; 
+      });
+      
+      var awardsCompleted = awardsStarted.forEach(function(current) {
+        if(isComplete(current)) {
+          return current;  
+        }
+      });
+        
+      //This would work much better if the award data were structured as an array of requirements
+      // right now we will just return the first one in the array. we need to define an order somehow
+      return awardsCompleted[0];
+    }
   }
 ]);
